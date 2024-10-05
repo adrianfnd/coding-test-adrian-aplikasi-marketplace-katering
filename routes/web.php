f5eb12cac98ce52cf3e3b4a92f2d068a1117dc74
adrianfnd <fernanda.adrian2@gmail.com>
2024-10-05 15:10:34 +0700

    create middleware for auth and roles

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MenuController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +18,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register-merchant', [AuthController::class, 'showRegisterFormMerchant'])->name('register.merchant');
+Route::get('/register-customer', [AuthController::class, 'showRegisterFormCustomer'])->name('register.customer');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('menu', [MenuController::class, 'index'])->name('menu');
+    Route::get('menu-create', [MenuController::class, 'create'])->name('menu.create');
+    Route::post('menu-store', [MenuController::class, 'store'])->name('menu.store');
+    Route::get('menu/{menu}/edit', [MenuController::class, 'edit'])->name('menu.edit');
+    Route::put('menu/{menu}', [MenuController::class, 'update'])->name('menu.update');
+    Route::delete('menu/{menu}', [MenuController::class, 'destroy'])->name('menu.destroy');
+});;
